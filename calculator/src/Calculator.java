1681d2433310a5217198a62e4483768533f7b46c
Dan Rosen <dan@marakana.com>
2012-12-17 13:35:10 -0800

    added some tokenizing code

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -2,14 +2,34 @@
 public class Calculator {
 
 	public static void main(String[] args) {
-		// 1. make sure args.length == 1 and get the expression from args[0]
-		// 2. split the expression into tokens, using whitespace as delimiter
-		// 3. for each token:
-		//    - if it's a number, push it on the stack
-		//    - if it's an operator:
-		//        pop two operands off the stack
-		//        do the operation
-		//        push the result back on the stack
+		if (args.length != 1) {
+			System.err.println("Usage: Calculator <expression>");
+			return;
+		}
+
+		for (String token : args[0].split(" ")) {
+			try {
+				//    - if it's a number, push it on the stack
+				int number = Integer.parseInt(token);
+				System.out.println(number + " is a number");
+			} catch (NumberFormatException e) {
+				//    - if it's an operator:
+				//        pop two operands off the stack
+				//        do the operation
+				//        push the result back on the stack
+				switch (token) {
+				case "+":
+				case "-":
+				case "*":
+				case "/":
+					System.out.println(token + " is an operator");
+					break;
+				default:
+					System.out.println(token + " is garbage");
+				}
+			}
+		}
+
 		// 4. print out whatever is left on the stack
 	}
 
