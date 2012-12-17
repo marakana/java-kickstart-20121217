312e6db18d9b4e47598e1511543b33bd032f7a1b
Dan Rosen <dan@marakana.com>
2012-12-17 15:05:33 -0800

    refactored the remainder of the procedural-style calculator

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -1,6 +1,5 @@
 import java.util.Stack;
 
-
 public class Calculator {
 
 	public static void main(String[] args) {
@@ -8,44 +7,72 @@ public class Calculator {
 			System.err.println("Usage: Calculator <expression>");
 			return;
 		}
-
-		Stack<Integer> stack = new Stack<>();
-		for (String token : args[0].split(" ")) {
-			try {
-				// if the token is a number, push it on the stack
-				stack.push(Integer.parseInt(token));
-			} catch (NumberFormatException e) {
-				handleOperator(token, stack);
-			}
-		}
-
-		System.out.println(stack.pop());
+		System.out.println(calculate(args[0]));
 	}
 
-	private static void handleOperator(String token, Stack<Integer> stack) {
+	/**
+	 * Calculate the result of a postfix expression.
+	 * @param expression
+	 * @return result
+	 */
+	public static int calculate(String expression) {
+		Stack<Integer> stack = new Stack<>();
+		for (String token : expression.split(" ")) {
+			if (!handleNumber(token, stack) && !handleOperator(token, stack)) {
+				throw new IllegalArgumentException("garbage in input: " + token);
+			}
+		}
+		return stack.pop();
+	}
+
+	/**
+	 * If the token is a number, push it on the stack and return true,
+	 * otherwise do nothing and return false.
+	 * @param token
+	 * @param stack
+	 * @return whether the token was a number or not
+	 */
+	public static boolean handleNumber(String token, Stack<Integer> stack) {
+		try {
+			stack.push(Integer.parseInt(token));
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	/**
+	 * If the token is an operator, pop two operands off the stack,
+	 * perform the operation, push the result on the stack and return true;
+	 * otherwise do nothing and return false.
+	 * @param token
+	 * @param stack
+	 * @return whether the token was an operator or not
+	 */
+	public static boolean handleOperator(String token, Stack<Integer> stack) {
 		switch (token) {
 		case "+": {
 			int rhs = stack.pop(), lhs = stack.pop();
 			stack.push(lhs + rhs);
-			break;
+			return true;
 		}
 		case "-": {
 			int rhs = stack.pop(), lhs = stack.pop();
 			stack.push(lhs - rhs);
-			break;
+			return true;
 		}
 		case "*": {
 			int rhs = stack.pop(), lhs = stack.pop();
 			stack.push(lhs * rhs);
-			break;
+			return true;
 		}
 		case "/": {
 			int rhs = stack.pop(), lhs = stack.pop();
 			stack.push(lhs / rhs);
-			break;
+			return true;
 		}
 		default:
-			System.out.println(token + " is garbage");
+			return false;
 		}
 	}
 
