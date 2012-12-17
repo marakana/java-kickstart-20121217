6a2c00047bb0ccc4e1259233a5fd95c6c79506c8
Dan Rosen <dan@marakana.com>
2012-12-17 14:30:48 -0800

    extracted handleNumber...

--- calculator/src/Calculator.java
+++ calculator/src/Calculator.java
@@ -15,35 +15,38 @@ public class Calculator {
 				// if the token is a number, push it on the stack
 				stack.push(Integer.parseInt(token));
 			} catch (NumberFormatException e) {
-				// if the token is an operator, pop two operands and push the result
-				switch (token) {
-				case "+": {
-					int rhs = stack.pop(), lhs = stack.pop();
-					stack.push(lhs + rhs);
-					break;
-				}
-				case "-": {
-					int rhs = stack.pop(), lhs = stack.pop();
-					stack.push(lhs - rhs);
-					break;
-				}
-				case "*": {
-					int rhs = stack.pop(), lhs = stack.pop();
-					stack.push(lhs * rhs);
-					break;
-				}
-				case "/": {
-					int rhs = stack.pop(), lhs = stack.pop();
-					stack.push(lhs / rhs);
-					break;
-				}
-				default:
-					System.out.println(token + " is garbage");
-				}
+				handleOperator(token, stack);
 			}
 		}
 
 		System.out.println(stack.pop());
+	}
+
+	private static void handleOperator(String token, Stack<Integer> stack) {
+		switch (token) {
+		case "+": {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs + rhs);
+			break;
+		}
+		case "-": {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs - rhs);
+			break;
+		}
+		case "*": {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs * rhs);
+			break;
+		}
+		case "/": {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs / rhs);
+			break;
+		}
+		default:
+			System.out.println(token + " is garbage");
+		}
 	}
 
 }
