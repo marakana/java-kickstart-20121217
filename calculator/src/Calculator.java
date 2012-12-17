81fb0b466c9f19f715d14d490048044c61f01442
Dan Rosen <dan@marakana.com>
2012-12-17 13:06:06 -0800

    added calculator project outline

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// 1. make sure args.length == 1 and get the expression from args[0]
+		// 2. split the expression into tokens, using whitespace as delimiter
+		// 3. for each token:
+		//    - if it's a number, push it on the stack
+		//    - if it's an operator:
+		//        pop two operands off the stack
+		//        do the operation
+		//        push the result back on the stack
+		// 4. print out whatever is left on the stack
+	}
+
+}
