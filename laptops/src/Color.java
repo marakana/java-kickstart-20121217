496724f06e1aa6778309f96f5dd9b06ec1f1cf63
Dan Rosen <dan@marakana.com>
2012-12-18 14:32:33 -0800

    implemented polymorphic toString for colors

--- laptops/src/Color.java
+++ laptops/src/Color.java
@@ -1,3 +1,22 @@
 public enum Color {
-	BLACK, RED, BLUE, SILVER
+	BLACK {
+		public String toString() {
+			return "Black";
+		}
+	},
+	RED {
+		public String toString() {
+			return "Red";
+		}
+	},
+	BLUE {
+		public String toString() {
+			return "Blue";
+		}
+	},
+	SILVER {
+		public String toString() {
+			return "Silver";
+		}
+	};
 }
