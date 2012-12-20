7791109f68afd5c28eab814f612fcaf84aaff2c3
Dan Rosen <dan@marakana.com>
2012-12-19 17:02:55 -0800

    handle exceptions using try-with-resources blocks

--- clientserver/src/com/marakana/clientserver/Client.java
+++ clientserver/src/com/marakana/clientserver/Client.java
@@ -1,15 +1,19 @@
 package com.marakana.clientserver;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception {
-		Socket server = new Socket(InetAddress.getLocalHost(), 31337);
-		PrintWriter out = new PrintWriter(server.getOutputStream());
-		out.println("hello, world");
+	public static void main(String[] args) {
+		try (Socket server = new Socket(InetAddress.getLocalHost(), 31337);
+		     PrintWriter out = new PrintWriter(server.getOutputStream())) {
+			out.println("hello, world");
+			out.flush();
+		} catch (IOException e) {
+		}
 	}
 
 }
